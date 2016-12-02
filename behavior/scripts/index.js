f6158c221a9ce75ea94966ac16c677ae1afd8a5d
alexheffesse <alex.heffesse@gmail.com>
2016-12-02 13:48:02 -0500

    undo

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -1,80 +1,55 @@
-'use strict'
-
-  const firstOfEntityRole = function(message, entity, role) {
-  role = role || 'generic';
-
-  const slots = message.slots
-  const entityValues = message.slots[entity]
-  const valsForRole = entityValues ? entityValues.values_by_role[role] : null
-
-  return valsForRole ? valsForRole[0] : null
-}
-exports.handle = function handle(client) {
+exports.handle = function handle(client) => {
+  // Create steps
   const sayHello = client.createStep({
     satisfied() {
       return Boolean(client.getConversationState().helloSent)
     },
+
     prompt() {
       client.addResponse('welcome')
       client.addResponse('provide/documentation', {
         documentation_link: 'http://docs.init.ai',
       })
       client.addResponse('provide/instructions')
+
       client.updateConversationState({
         helloSent: true
       })
+
       client.done()
     }
   })
+
   const untrained = client.createStep({
     satisfied() {
       return false
     },
+
     prompt() {
       client.addResponse('apology/untrained')
       client.done()
     }
   })
+
   const handleGreeting = client.createStep({
     satisfied() {
       return false
     },
-    const collectCity = client.createStep({
-      satisfied() {
-        return Boolean(client.getConversationState().weatherCity)
-      },
-      prompt() {
-        console.log('Need to ask user for city')
-        client.done()
-      },
-    })
-    const provideWeather = client.createStep({
-      satisfied() {
-        return false
-      },
-      prompt() {
-        let weatherData = {
-      temperature: 69,
-      condition: 'doom',
-      city: client.getConversationState().weatherCity.value,
-    }
-        client.addResponse('app:response:name:provide_weather/current', weatherData)
-        client.done()
-      },
-    })
-      prompt() {
-        client.addTextResponse('Hey! Do you like contemporary art?')
-        client.done()
+
+    prompt() {
+      client.addTextResponse('What would you like to know?')
+      client.done()
     }
   })
+
   const handleGoodbye = client.createStep({
     satisfied() {
       return false
     },
-  })
-  prompt() {
-    client.addTextResponse('Enough. To continue the conversation, please send questions to alex.heffesse@gmail.com')
-    client.done()
+
+    prompt() {
+      client.addTextResponse('Enough. To continue the conversation, please send questions to alex.heffesse@gmail.com')
+      client.done()
     }
   })
 
@@ -83,20 +58,25 @@ exports.handle = function handle(client) {
     goodbye: 'goodbye',
     greeting: 'greeting',
     },
-})
     autoResponses: {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
-  streams: {
-    goodbye: handleGoodbye,
-    greeting: handleGreeting,
-    main: 'onboarding',
-    onboarding: [sayHello],
-    end: [untrained],
-    main: 'hi',
-    hi: [sayHello],
-    getWeather: [collectCity, provideWeather],
-      }
+    streams: {
+      goodbye: handleGoodbye,
+      greeting: handleGreeting,
+      main: 'onboarding',
+      onboarding: [sayHello],
+      end: [untrained],
     },
   })
+  const handleGoodbye = client.createStep({
+  satisfied() {
+    return false
+  },
+
+  prompt() {
+    client.addResponse('goodbye')
+    client.done()
+  }
+})
 }
